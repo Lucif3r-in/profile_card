1d56eb242489ec3e26b288a3bf6744f6561eb2a9
Kasi Reeves <Kasirocswell@Rocketmail.com>
2022-08-04 12:13:29 -0700

    profile-card

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Profile from './Components/Profile.js'
+
+function App() {
+  return (
+    <div className="absolute w-screen h-screen bg-[#19A1AE]">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Profile.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import banner from './images/bg-pattern-card.svg'
+import pic from './images/image-victor.jpg'
+
+const Profile = () => {
+  return (
+    <div className='absolute flex w-screen h-screen'>
+        <div className='w-[326px] h-[374px] bg-white mx-auto my-auto rounded-2xl overflow-hidden'>
+            <div className=''>
+            <img className='top-0 z-10' src={banner} alt=''></img>
+            </div>
+            <div className='relative'>
+            <img className='z-0 mx-auto mt-[-45px] rounded-full border-[5px] border-white' src={pic} alt=''></img>
+            </div>
+            <div className='flex-col'>
+                <h2 className='text-center text-[18px] font-main font-bold mt-[12px]'>Victor Crest <span className='font-light text-[#6B7082] ml-2'>26</span></h2>
+                <h2 className='text-center font-main text-[14px] mt-[12px]'>London</h2>
+                <div className='border mt-[40px]'></div>
+                <div className='flex flex-row'>
+                    <div className='flex-col'>
+                        <p className='text-[#2E3349] text-[18px] font-main font-bold text-center ml-[41px] mt-[12px]'>80K</p>
+                        <p className='text-[10px] text-[#6B7082] font-main ml-[41px]'>Followers</p>
+                    </div>
+                    <div className='flex-col'>
+                        <p className='text-[#2E3349] text-[18px] font-main font-bold text-center ml-[61px] mt-[12px]'>803K</p>
+                        <p className='text-[10px] text-[#6B7082] font-main ml-[70px]'>Likes</p>
+                    </div>
+                    <div className='flex-col'>
+                        <p className='text-[#2E3349] text-[18px] font-main font-bold text-center ml-[55px] mt-[12px]'>1.4K</p>
+                        <p className='text-[10px] text-[#6B7082] font-main ml-[57px]'>Photos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
